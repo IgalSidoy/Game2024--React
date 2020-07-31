9f75c04a9a504481edee12d33c38a7cf282f6a23
igal sidoy <igal.sidoy@gmail.com>
2020-07-31 12:30:44 +0300

    missing files

--- src/Containers/Board/Board.jsx
+++ src/Containers/Board/Board.jsx
@@ -3,76 +3,207 @@ import classes from "./Board.module.css";
 import Number from "../../Components/Number/Number";
 import colors from "../../Utili/numberColor";
 export default class Board extends Component {
+  constructor() {
+    super();
+  }
+
+  state = {
+    board: [
+      [2, 2, "", ""],
+      ["", "", "", ""],
+      ["", "", "", ""],
+      ["", "", "", 16],
+    ],
+  };
+
+  initBoard() {
+    let board = [
+      ["", "", "", ""],
+      ["", "", "", ""],
+      ["", "", "", ""],
+      ["", "", 2, 2],
+    ];
+
+    this.setState({ board });
+  }
+
+  insertNumber(board, number, row_i, col_i) {
+    board[row_i][col_i] = number;
+    return board;
+  }
+  addNumberBoBoard(board) {
+    let empty = [];
+    for (let row_i in board) {
+      for (let col_i in board[row_i]) {
+        if (board[row_i][col_i] === "") {
+          console.log(row_i, col_i, "empty");
+          empty.push(row_i + "-" + col_i);
+        }
+      }
+    }
+    let selected = empty.length 
+    console.log(empty);
+  }
+
+  componentDidMount() {
+    window.addEventListener("keypress", (e) => {
+      this.onKeyDownHandler(e);
+    });
+
+    this.addNumberBoBoard(this.state.board);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("keypress", (e) => {});
+  }
   render() {
     return (
       <div className={classes.container}>
-        <Number
-          number={2}
-          color={colors[2].color}
-          backgroundColor={colors[2].backgroundColor}
-          fontSize={colors[2].fontSize}
-        ></Number>
-        <Number
-          number={4}
-          color={colors[4].color}
-          backgroundColor={colors[4].backgroundColor}
-          fontSize={colors[4].fontSize}
-        ></Number>
-        <Number
-          number={8}
-          color={colors[8].color}
-          backgroundColor={colors[8].backgroundColor}
-          fontSize={colors[8].fontSize}
-        ></Number>
-        <Number
-          number={16}
-          color={colors[16].color}
-          backgroundColor={colors[16].backgroundColor}
-          fontSize={colors[16].fontSize}
-        ></Number>
-        <Number
-          number={32}
-          color={colors[32].color}
-          backgroundColor={colors[32].backgroundColor}
-          fontSize={colors[32].fontSize}
-        ></Number>
-        <Number
-          number={64}
-          color={colors[64].color}
-          backgroundColor={colors[64].backgroundColor}
-          fontSize={colors[64].fontSize}
-        ></Number>
-        <Number
-          number={128}
-          color={colors[128].color}
-          backgroundColor={colors[128].backgroundColor}
-          fontSize={colors[128].fontSize}
-        ></Number>
-        <Number
-          number={256}
-          color={colors[256].color}
-          backgroundColor={colors[256].backgroundColor}
-          fontSize={colors[256].fontSize}
-        ></Number>
-        <Number
-          number={512}
-          color={colors[512].color}
-          backgroundColor={colors[512].backgroundColor}
-          fontSize={colors[512].fontSize}
-        ></Number>
-        <Number
-          number={1024}
-          color={colors[1024].color}
-          backgroundColor={colors[1024].backgroundColor}
-          fontSize={colors[1024].fontSize}
-        ></Number>
-        <Number
-          number={2048}
-          color={colors[2048].color}
-          backgroundColor={colors[2048].backgroundColor}
-          fontSize={colors[2048].fontSize}
-        ></Number>
+        {this.state.board.map((line, key) => (
+          <div className={classes.row} key={key}>
+            {line.map((number, key2) => (
+              <div className={classes.col} key={key2}>
+                <Number
+                  number={number}
+                  backgroundColor={colors[number].backgroundColor}
+                  color={colors[number].color}
+                  fontSize={colors[number].fontSize}
+                ></Number>
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
     );
   }
+  onKeyDownHandler(e) {
+    let key = e.key.toLowerCase();
+    //left
+    if (key === "a") {
+      this.moveBoardHandler("left");
+      return;
+    }
+    //right
+    if (key === "d") {
+      this.moveBoardHandler("right");
+      return;
+      return;
+    }
+    //up
+    if (key === "w") {
+      this.moveBoardHandler("up");
+      return;
+    }
+    //down
+    if (key === "s") {
+      this.moveBoardHandler("down");
+      return;
+    }
+  }
+  printBoard(board) {
+    for (let line in board) {
+      let line_str = "";
+      for (let number in board[line]) {
+        let num = board[line][number];
+        if (num.toString().length === 0) line_str += num + "    |  ";
+        if (num.toString().length === 1) line_str += num + "   |  ";
+        if (num.toString().length === 2) line_str += num + "  |  ";
+        if (num.toString().length === 3) line_str += num + " |  ";
+        if (num.toString().length === 4) line_str += num + "|  ";
+      }
+      console.log(line_str);
+    }
+  }
+  moveBoardHandler(dir) {
+    console.clear();
+    let board = JSON.parse(JSON.stringify(this.state.board));
+
+    if (dir === "right") {
+      for (let row_index in board) {
+        board[row_index] = this.moveRowRight(board[row_index], dir);
+      }
+    }
+    if (dir === "left") {
+      for (let row_index in board) {
+        board[row_index] = this.moveRowLeft(board[row_index], dir);
+      }
+    }
+    if (dir === "up") {
+      for (let col_1 in board) {
+        board = this.moveUp(board, col_1);
+      }
+    }
+    if (dir === "down") {
+      for (let col_1 in board) {
+        board = this.moveDown(board, col_1);
+      }
+    }
+    this.setState({ board });
+  }
+  moveRowRight = (row, dir) => {
+    for (let loop in row) {
+      for (let i = row.length; i > 0; i--) {
+        row = this.checkTwoNumber(row, dir, i - 1, i);
+      }
+    }
+    return row;
+  };
+  moveRowLeft = (row, dir) => {
+    for (let loop in row) {
+      for (let i = 0; i < row.length - 1; i++) {
+        row = this.checkTwoNumber(row, dir, i, i + 1);
+      }
+    }
+    return row;
+  };
+  moveUp = (board, col_i) => {
+    let row = this.colToRow(board, col_i);
+    row = this.moveRowLeft(row, "left");
+    board = this.rowToCol(board, col_i, row);
+    return board;
+  };
+  moveDown = (board, col_i) => {
+    let row = this.colToRow(board, col_i);
+    row = this.moveRowLeft(row, "right");
+    board = this.rowToCol(board, col_i, row);
+    return board;
+  };
+  colToRow = (board, col) => {
+    let row = [];
+    for (let r in board) {
+      row.push(board[r][col]);
+    }
+    return row;
+  };
+  rowToCol = (board, col_i, col) => {
+    for (let r in board) {
+      board[r][col_i] = col[r];
+    }
+    return board;
+  };
+  checkTwoNumber = (row, dir, left_i, right_i) => {
+    let right_num = row[right_i];
+    let left_num = row[left_i];
+    //console.log("row", row, "left", left_num, "right", right_num);
+    if (dir === "right") {
+      if (right_num === left_num) {
+        row[right_i] = right_num * 2;
+        row[left_i] = "";
+      }
+      if (right_num === "") {
+        row[right_i] = left_num;
+        row[left_i] = "";
+      }
+    }
+    if (dir === "left") {
+      if (right_num === left_num) {
+        row[left_i] = left_num * 2;
+        row[right_i] = "";
+      }
+      if (left_num === "") {
+        row[left_i] = right_num;
+        row[right_i] = "";
+      }
+    }
+    return row;
+  };
 }
--- src/Utili/numberColor.js
+++ src/Utili/numberColor.js
@@ -1,4 +1,5 @@
 const colors = {
+  "": { backgroundColor: "#cdc0b4", color: "#776e65", fontSize: "25px" },
   2: { backgroundColor: "#eee4da", color: "#776e65", fontSize: "25px" },
   4: { backgroundColor: "#ede0c8", color: "#776e65", fontSize: "25px" },
   8: { backgroundColor: "#f2b179", color: "#f9f6f2", fontSize: "25px" },
@@ -10,8 +11,6 @@ const colors = {
   512: { backgroundColor: "#edc850", color: "#f9f6f2", fontSize: "25px" },
   1024: { backgroundColor: "#edc53f", color: "#f9f6f2", fontSize: "20px" },
   2048: { backgroundColor: "#edc22e", color: "#f9f6f2", fontSize: "20px" },
-
-  // font-size: ;
 };
 
 export default colors;
